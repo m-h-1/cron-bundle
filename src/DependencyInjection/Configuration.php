457c70a8e4a21705777e4f1219e907ceeb7e1e8a
m-h-1 <dev@hoefges.net>
2021-10-22 10:22:55 +0200

    add lock prefix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace MH1\CronBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\BooleanNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\ScalarNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -23,10 +24,22 @@ class Configuration implements ConfigurationInterface
         $cronjobLogServiceClassNode = new ScalarNodeDefinition('log_service');
         $cronjobLogServiceClassNode->defaultNull();
 
+        $cronjobCheckIntervalNode = new ScalarNodeDefinition('check_interval');
+        $cronjobCheckIntervalNode->defaultNull();
+
+        $cronjobExecutionTimeZoneNode = new ScalarNodeDefinition('execution_time_zone');
+        $cronjobExecutionTimeZoneNode->defaultValue(1000000);
+
+        $cronjobLockPrefixNode = new ScalarNodeDefinition('lock_prefix');
+        $cronjobLockPrefixNode->defaultValue('');
+
         /** @var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->getRootNode();
         $rootNode->append($cronjobServiceClassNode);
         $rootNode->append($cronjobLogServiceClassNode);
+        $rootNode->append($cronjobCheckIntervalNode);
+        $rootNode->append($cronjobExecutionTimeZoneNode);
+        $rootNode->append($cronjobLockPrefixNode);
 
         return $treeBuilder;
     }
